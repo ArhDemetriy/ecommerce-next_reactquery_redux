40fea44ce34924bad707d1177fa8fb8e49f583de
Dmitriy Nazimov <ArhDemetriy@GMail.com>
2025-12-20 19:35:28 +0800

    try fix back error 02

--- app/(header)/(hero)/catalog/[slug]/page.tsx
+++ app/(header)/(hero)/catalog/[slug]/page.tsx
@@ -11,30 +11,30 @@ interface Props {
 }
 
 export const dynamicParams = true;
-export async function generateStaticParams() {
-  return [];
+// export async function generateStaticParams() {
+//   return [];
 
-  // try {
-  //   const queryClient = getQueryClient();
+//   // try {
+//   //   const queryClient = getQueryClient();
 
-  //   const allCategories = await queryClient.fetchQuery(categoriesOptions());
-  //   if (!allCategories?.length) return [];
+//   //   const allCategories = await queryClient.fetchQuery(categoriesOptions());
+//   //   if (!allCategories?.length) return [];
 
-  //   const uuids: Set<(typeof allCategories)[number]['uuid']> = new Set();
-  //   (function extractSlugs(categories: typeof allCategories) {
-  //     for (const { uuid, children } of categories) {
-  //       if (uuids.has(uuid)) continue;
-  //       uuids.add(uuid);
-  //       if (children?.length) extractSlugs(children);
-  //     }
-  //   })(allCategories);
+//   //   const uuids: Set<(typeof allCategories)[number]['uuid']> = new Set();
+//   //   (function extractSlugs(categories: typeof allCategories) {
+//   //     for (const { uuid, children } of categories) {
+//   //       if (uuids.has(uuid)) continue;
+//   //       uuids.add(uuid);
+//   //       if (children?.length) extractSlugs(children);
+//   //     }
+//   //   })(allCategories);
 
-  //   return Array.from(uuids).map(uuid => ({ slug: uuid }) satisfies Props);
-  // } catch (error) {
-  //   console.error('Ошибка при генерации статических параметров категорий:', error);
-  //   return [];
-  // }
-}
+//   //   return Array.from(uuids).map(uuid => ({ slug: uuid }) satisfies Props);
+//   // } catch (error) {
+//   //   console.error('Ошибка при генерации статических параметров категорий:', error);
+//   //   return [];
+//   // }
+// }
 
 export async function generateMetadata({ params }: { params: Promise<Props> }) {
   const queryClient = getQueryClient();
